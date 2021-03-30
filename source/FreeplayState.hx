4c65d989fa0e32d0e16d457da2df4b49b30a2c0a
reithegoat <clxgamer12@gmail.com>
2021-03-29 21:50:46 -0400

    Added Moon High as a track

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -80,7 +80,7 @@ class FreeplayState extends MusicBeatState
 			addWeek(['Senpai', 'Roses', 'Thorns'], 6, ['senpai', 'senpai', 'spirit']);
 
 		if(StoryMenuState.weekUnlocked[7] || isDebug)
-			addWeek(['SongOne'],7,['pico']);
+			addWeek(['SongOne','Moon-High'],7,['pico']);
 
 		// LOAD MUSIC
 
