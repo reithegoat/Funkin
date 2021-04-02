8e2d9c055578722ddbcebf4e2d2499413b7921c9
reithegoat <clxgamer12@gmail.com>
2021-04-02 13:49:42 -0400

    Kade's Note Fix

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -1005,25 +1005,17 @@ class ChartingState extends MusicBeatState
 
 	function deleteNote(note:Note):Void
 	{
-		trace(_song.notes[curSection].sectionNotes);
-		for (n in 0..._song.notes[curSection].sectionNotes.length)
+		for (i in _song.notes[curSection].sectionNotes)
 		{
-			var i = _song.notes[curSection].sectionNotes[n];
-			if (i == null)
-				continue;
-			if ((i[0] == note.strumTime + (note.strumTime == 0 ? 0 : 1) 
-				? true : i[0] == note.strumTime) 
-				&& i[1] % 4 == note.noteData)
-				// Why does it do this?
-				// I DONT FUCKING KNOW!!!!!!!!!!!!!!
+			if (i[0] == note.strumTime && i[1] % 4 == note.noteData)
 			{
-				trace('GAMING');
 				_song.notes[curSection].sectionNotes.remove(i);
 			}
 		}
-
+	
 		updateGrid();
 	}
+
 
 	function clearSection():Void
 	{
